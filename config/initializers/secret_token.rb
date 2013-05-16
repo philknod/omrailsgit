48658172e51991555875e0c3d44ac52bfef63100
philknod <philknodel@gmail.com>
2013-05-16 16:45:22 -0700

    Add user model with Devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = '5385754245b0eb58a2acfff0afa1bdef5f758f6a4b7c3db7dc956bfa1fc945e28c69fc1e72b17d7fe5d89d3dbe2bae977106eed7617ea9eaedb98ea9d07b22eb'
